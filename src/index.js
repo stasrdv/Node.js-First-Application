7527c0ec61c8e69609b0012407e4570c26583163
Stas Rudevitsky <stasru@codevalue.net>
2019-01-07 16:22:38 +0200

    404 and 500 pages  implemented

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 let express = require("express");
 let app = express();
+let path = require("path");
 
 let personRoute = require("./routes/person");
+
+// Middleware
+app.use((req, res, next) => {
+  console.log(`${new Date().toString()} =>  ${req.originalUrl}`);
+  next();
+});
+
 app.use(personRoute);
 app.use(express.static("public"));
-const port = process.env.port || 3000;
+
+// Handler for 404 - Not found
+app.use((req, res, next) => {
+  res.status(404).send("Not Found");
+});
+// Handler for Error 500
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.sendFile(path.join(__dirname, "../public/500.html"));
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.info(`Server has started on ${PORT}`));
--- src/routes/person.js
+++ src/routes/person.js
@@ -15,4 +15,7 @@ router.get("/person/:name", (req, res) => {
   res.send(`You have requested a person ${req.query.name}`);
 });
 
+router.get("/error", (req, res) => {
+  throw new Error("This is a forced error.");
+});
 module.exports = router;
