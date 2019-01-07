e0deed449d075a8bf555d597cf07484cd5ed4006
Stas Rudevitsky <stasru@codevalue.net>
2019-01-07 15:45:28 +0200

    routes implemented

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 let express = require("express");
 let app = express();
 
+let personRoute = require("./routes/person");
+app.use(personRoute);
 app.use(express.static("public"));
-
 const port = process.env.port || 3000;
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.info(`Server has started on ${PORT}`));
---
+++ src/routes/person.js
@@ -0,0 +1,18 @@
+let express = require("express");
+let router = express.Router();
+
+//QueryString => query property on request object
+router.get("/person", (req, res) => {
+  if (req.query.name) {
+    res.send(`You have requested a person named ${req.query.name}`);
+  } else {
+    res.send("You have requested a person");
+  }
+});
+
+//Params property on request object
+router.get("/person/:name", (req, res) => {
+  res.send(`You have requested a person ${req.query.name}`);
+});
+
+module.exports = router;
