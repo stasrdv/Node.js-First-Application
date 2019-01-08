4af5cfab819e009fbdea77f70a0e5c7713fd3d2c
Stas Rudevitsky <stasru@codevalue.net>
2019-01-08 14:24:12 +0200

    put,delete,update methods implemented

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -2,9 +2,8 @@ let CustomerModel = require("../models/customer.model");
 let express = require("express");
 let router = express.Router();
 
-//  Create a new customer
+// Create a new customer
 router.post("/customer", (req, res) => {
-  console.log(req.body);
   if (!req.body) {
     return res.status(400).send("Request body is empty");
   }
@@ -15,7 +14,6 @@ router.post("/customer", (req, res) => {
       if (!doc || doc.length == 0) {
         return res.status(500).send(doc);
       } else {
-        console.log(doc);
         return res
           .status(201)
           .send(
@@ -28,6 +26,61 @@ router.post("/customer", (req, res) => {
     .catch(err => {
       res.status(500).json(err);
     });
+});
+
+// GET customer
+router.get("/customer", (req, res) => {
+  if (!req.query.email) {
+    res.status(400).send("Missing url parameter: email");
+  } else {
+    CustomerModel.findOne({
+      email: req.query.email
+    })
+      .then(doc => {
+        res.json(doc);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  }
+});
+
+//UPDATE
+router.put("/customer", (req, res) => {
+  if (!req.query.email) {
+    res.status(400).send("Missing url parameter: email");
+  } else {
+    CustomerModel.findOneAndUpdate(
+      {
+        email: req.query.email
+      },
+      req.body,
+      {
+        new: true
+      }
+    )
+      .then(doc => {
+        res.json(doc);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  }
+});
+
+// DELETE
+router.delete("/customer", (req, res) => {
+  if (!req.query.email) {
+    res.status(400).send("Missing url parameter: email");
+  } else {
+    CustomerModel.findOneAndRemove({ email: req.query.email })
+      .then(doc => {
+        res.json(doc);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  }
 });
 
 module.exports = router;
