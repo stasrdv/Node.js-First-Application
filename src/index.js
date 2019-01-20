71ad3eff354f48668c1ca78cdd1124fef82aafa1
Stas Rudevitsky <stasru@codevalue.net>
2019-01-20 13:21:44 +0200

    refactoring

--- src/index.js
+++ src/index.js
@@ -19,10 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expres.static("public"));
 
-app.use("/entry", (req, res) => {
-  res.redirect("/");
-});
-app.use("/home", (req, res) => {
+app.all("*", (req, res) => {
   res.redirect("/");
 });
 
@@ -33,12 +30,6 @@ app.use(
 );
 // Apply routes
 app.use(loginRoute, registerRoute, getItems);
-
-// Handler for 404 - Not found
-// app.use((req, res, next) => {
-//   res.status(404).json("Not Found");
-// });
-// Handler for Error
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.info(`Server has started on ${PORT}`));
