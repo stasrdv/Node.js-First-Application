4e55d849e2b5400780189f39763c1fe70fb45ea8
Stas Rudevitsky <stasru@codevalue.net>
2019-01-20 13:56:33 +0200

    refactoring

--- src/index.js
+++ src/index.js
@@ -8,35 +8,24 @@ let registerRoute = require("./routes/register");
 let loginRoute = require("./routes/auth");
 let getItems = require("./routes/items");
 // let verify = require("./routes/verify");
-
 let app = expres();
-
 // cors
 app.use(cors());
-
 // BodyParser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expres.static("public"));
+
+// Apply routes
+app.use(loginRoute, registerRoute, getItems);
+app.get("/*", (req, res) => {
+  res.redirect("/");
+});
 app.use(
   expressJwt({ secret: "i31GOVwz5K0W" }).unless({
     path: ["/auth", "/register"]
   })
 );
-// Apply routes
-app.use(loginRoute, registerRoute, getItems);
-
-app.use("/entry", (req, res) => {
-  res.redirect("/");
-});
-app.use("/home", (req, res) => {
-  res.redirect("/");
-});
-// Handler for 404 - Not found
-// app.use((req, res, next) => {
-//   res.status(404).json("Not Found");
-// });
-// Handler for Error
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.info(`Server has started on ${PORT}`));
