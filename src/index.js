4f4c6f0092f748382a7f7ac4c9f7b6f50cd10e11
Stas Rudevitsky <stasru@codevalue.net>
2019-01-20 16:15:26 +0200

    refactoring

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@ const expressJwt = require("express-jwt");
 let registerRoute = require("./routes/register");
 let loginRoute = require("./routes/auth");
 let getItems = require("./routes/items");
-// let verify = require("./routes/verify");
+
 let app = expres();
+
 // cors
 app.use(cors());
+
 // BodyParser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -21,11 +23,15 @@ app.use(
     path: ["/auth", "/register", "/verify"]
   })
 );
-
 // Apply routes
 app.use(loginRoute, registerRoute, getItems);
-app.get("/*", (req, res) => {
-  res.redirect("/");
+
+// Handler for Error
+app.use((err, req, res, next) => {
+  if (err) {
+    res.send(err.status).json(err.code);
+  }
+  // res.sendFile(path.join(__dirname, "../public/500.html"));
 });
 
 const PORT = process.env.PORT || 3000;
